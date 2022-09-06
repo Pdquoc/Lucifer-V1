6396957d5b66f5d0e21373cb433f386071337b6d
Pdquoc <108917841+Pdquoc@users.noreply.github.com>
2022-09-06 19:02:56 +0700

    Update lucifer.py

--- lucifer.py
+++ lucifer.py
@@ -89,7 +89,7 @@ def menu():
 def main():
     menu()
     while(True):
-        cnc = input('''\x1b[38;5;255m[ \x1b[38;5;228mroot\x1b[38;5;255m@\x1b[38;5;207mLucifer \x1b[38;5;255m] : ''')
+        cnc = input('''\x1b[38;5;255m[ \x1b[38;5;228mroot\x1b[38;5;255m@\x1b[38;5;207mLucifer \x1b[38;5;255m]# ''')
         if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
             layer7()
         elif cnc == "layer4" or cnc == "LAYER4" or cnc == "L4" or cnc == "l4":
